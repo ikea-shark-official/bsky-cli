3e89bcf34e492ee40300c2c7319ccb3e04df3177
zo <81495842+ikea-shark-official@users.noreply.github.com>
2024-11-28 19:05:20 -0500

    remove account switching logic
    it introduced more bugs than it was worth, would've needed some heady
    changes to truly fix, and nobody but me would have used it
    
    kinda just a bad feature

--- src/bsky.ts
+++ src/bsky.ts
@@ -2,13 +2,14 @@ import { AtpAgent } from "@atproto/api";
 import fs from "node:fs";
 import os from "node:os";
 import path from "node:path"
-import { Argument, Command } from "commander";
+import { Command } from "commander";
 import process from "node:process";
 import enquirer from "enquirer";
 const { prompt } = enquirer;
 
 import { exit } from "./common.ts";
 import { LocationInfo, PostData, makePost } from "./post.ts";
+
 
 /* ------------------------------------------------------------ */
 
@@ -27,25 +28,17 @@ const configFile =
   : path.join(os.homedir(), ".bsky-cli")
 
 type AccountInfo = { handle: string, did: string, password: string };
-type AuthStatus = { currentDid: string, lastAccountDid?: string } // stored in account_status.json
-
-type AuthInfo = { accounts: AccountInfo[] } & AuthStatus
 
 type BskyConfig = {
-  auth: AuthInfo,
+  auth: AccountInfo,
   history?: LocationInfo
 }
 
-function get_handle_by_did({accounts}: AuthInfo, did: string) {
-  return accounts.filter((a) => (a.did == did))[0].handle
-}
-function get_did_by_handle({accounts}: AuthInfo, handle: string) {
-  return accounts.filter((a) => (a.handle == handle))[0].did
+type BskyObjects = {
+  agent: AtpAgent
 }
 
-function get_active_account({accounts, currentDid}: AuthInfo): AccountInfo {
-  return accounts.filter((a) => (a.did == currentDid))[0];
-}
+type Bsky = BskyConfig & BskyObjects
 
 function load_config(): BskyConfig {
   return readJson(configFile)
@@ -58,48 +51,15 @@ function write_config({auth, history}: BskyConfig): void {
 
 /* ------------------------------------------------------------ */
 
-async function select_account_dialog(auth: AuthInfo, message: string): Promise<AccountInfo> {
-  const { removing } = await prompt({
-    type: 'select',
-    choices: auth.accounts.map((acc) => (acc.handle)),
-    initial: 0,
-    name: 'removing',
-    message: message
-  }) as { removing: string }
 
-  return auth.accounts.filter(acc => acc.handle == removing)[0]
-}
+async function bsky_login(account: AccountInfo): Promise<[AtpAgent, AccountInfo]> {
+  // login to bsky
+  const agent = new AtpAgent({
+    service: "https://bsky.social",
+  });
+  const res = await agent.login({ identifier: account.did, password: account.password });
 
-async function change_active_account(auth: AuthInfo): Promise<AuthInfo> {
-  const prev: AuthStatus = auth
-
-  const nextAccount = await select_account_dialog(auth, "select new account")
-
-  const next: AuthStatus = { currentDid: nextAccount.did };
-
-  if (next.currentDid == prev.currentDid) {
-    exit("you are already using " + nextAccount.handle);
-  }
-
-  const dids: string[] = auth.accounts.map((acc) => (acc.did))
-  if (!dids.includes(next.currentDid)) {
-    exit("given account name not found in auth.json")
-  }
-
-  next.lastAccountDid = prev.currentDid
-
-  console.log(
-    "switched account from " + get_handle_by_did(auth, prev.currentDid) +
-                      " to " + get_handle_by_did(auth, next.currentDid)
-  );
-  return { accounts: auth.accounts, ...next }
-}
-
-// print active accounts to terminal, for `bsky accounts`
-function list_accounts(auth: AuthInfo) {
-  for (const account of auth.accounts) {
-    console.log(account.handle)
-  }
+  return [agent, { ...account, handle: res.data.handle }]
 }
 
 async function ask_new_account(): Promise<AccountInfo> {
@@ -143,30 +103,10 @@ async function ask_new_account(): Promise<AccountInfo> {
   }
 }
 
-async function remove_account(auth: AuthInfo): Promise<AuthInfo> {
-  const removing = (await select_account_dialog(auth, 'select account to remove'))
-
-  const newAccounts = auth.accounts.filter((acc) => (acc.handle != removing.handle))
-
-  console.log(`removed ${removing} from account list`)
-  return { ...auth, accounts: newAccounts }
-}
-
 /* ------------------------------------------------------------ */
 
-async function bsky_login(auth: AuthInfo): Promise<AtpAgent> {
-  // login to bsky
-  const agent = new AtpAgent({
-    service: "https://bsky.social",
-  });
-  const account = get_active_account(auth)
-  await agent.login({ identifier: account.did, password: account.password });
-
-  return agent
-}
-
-async function post(postData: PostData, auth: AuthInfo): Promise<LocationInfo> {
-  const agent = await bsky_login(auth)
+async function post(postData: PostData, auth: AccountInfo): Promise<LocationInfo> {
+  const [agent, _] = await bsky_login(auth)
   const result = await makePost(postData, agent)
 
   // save post to history file
@@ -188,10 +128,7 @@ async function first_run() {
   const account = await ask_new_account()
 
   writeJson<BskyConfig>(configFile, {
-    auth: {
-      accounts: [account],
-      currentDid: account.did
-    }
+    auth: account
   })
 }
 
@@ -222,7 +159,7 @@ function makeCommand(
       const response = await prompt({
         type: 'input',
         name: 'text',
-        message: `${get_active_account(config.auth).handle}>`
+        message: `${config.auth.handle}>`
       }) as { text: string };
 
       // prevent the user from uploading blank strings
@@ -234,7 +171,8 @@ function makeCommand(
         text: response.text ,
       };
 
-      await post({ ...baseData, ...extraData(config) }, config.auth);
+      const lastPost = await post({ ...baseData, ...extraData(config) }, config.auth);
+      write_config({ ...config, history: lastPost})
     })
 }
 
@@ -255,42 +193,6 @@ makeCommand(
   "quote the last created post",
   ({ history }) => ({ quoting: require_history(history).post_info }),
 );
-
-program
-  .command("switch [account]")
-  .description("switch to the named account, or the last one used if unspecified")
-  .action(async () => {
-    await run_initial_setup_if_needed()
-    let { auth, history } = load_config()
-    auth = await change_active_account(auth)
-    write_config( { auth, history })
-  });
-
-program
-  .command('accounts')
-  .addArgument(new Argument('[command]').choices(['add', 'remove']))
-  .description("list accounts, or add/remove with 'accounts [add/remove]'")
-  .action(async (command) => {
-    let { auth, history } = load_config()
-
-    if (command == undefined) {
-      await run_initial_setup_if_needed()
-      list_accounts(auth)
-
-    } else if (command == 'add') {
-      if (needsFirstRun)
-        return (first_run())
-
-      const account = await ask_new_account()
-      auth.accounts.push(account)
-      write_config({ auth, history })
-
-    } else if (command == 'remove') {
-      await run_initial_setup_if_needed()
-      auth = await remove_account(auth)
-      write_config({ auth, history })
-    }
-  })
 
 program
   .command('init')
